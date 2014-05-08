956b3ce80baf830a1d9e00fdb398f4f6c58e45d0
Gregory Sheppard <gregory.sheppard@live.com>
2014-05-08 16:10:47 -0700

    Initial test for Daycare

---
+++ spec/features/add_pokemon_to_daycare_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+feature 'user can add a new Pokemon to the Daycare' do
+
+  # Acceptance
+  # Data a 'DaycarePokemon' encompasses:
+  # - Nickname
+  # - Species
+  # - Ability
+  # - Nature
+  # - IV Spread
+  # - Moveset (4 moves)
+  # - Gender
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  before :each do
+    sign_in_as(user)
+  end
+
+  scenario 'create valid Daycare Pokemon' do
+    visit '/daycare/'
+
+    click_on 'New'
+
+    fill_in 'Nickname', with: 'Steve'
+    fill_in 'Species', with: 'Bulbasaur'
+    fill_in 'Ability', with: 'Overgrow'
+    fill_in 'Nature', with: 'Hardy'
+    check 'Attack'
+    check 'Defense'
+    check 'Speed'
+    fill_in 'Move 1', with: 'Leaf Storm'
+    fill_in 'Move 2', with: 'Grassy Terrain'
+    fill_in 'Move 3', with: 'Petal Dance'
+    fill_in 'Move 4', with: 'Skull Bash'
+    choose 'Male'
+
+    click_on 'Add'
+
+    expect(user.daycare_pokemon.count).to eq(1)
+  end
+
+end
